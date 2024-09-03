781eb584f6cb9cbe837f3e12203defa2da9456fe
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-03 22:00:03 +0300

    'URLSessionHTTPClient' succeeds on HTTPURLResponse with Data

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/URLSessionHTTPClientTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/URLSessionHTTPClientTests.swift
@@ -77,6 +77,17 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertNotNil(resultErrorFor((data: anyData(), response: nonHTTPURLResponse(), error: nil)))
     }
     
+    func test_getFromURL_succeedsOnHTTPURLResponseWithData() {
+        let data = anyData()
+        let response = anyHTTPURLResponse()
+        
+        let receivedValues = resultValuesFor((data: data, response: response, error: nil))
+        
+        XCTAssertEqual(receivedValues?.data, data)
+        XCTAssertEqual(receivedValues?.response.url, response.url)
+        XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
+    }
+    
     // MARK: Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
         let configuration = URLSessionConfiguration.ephemeral
