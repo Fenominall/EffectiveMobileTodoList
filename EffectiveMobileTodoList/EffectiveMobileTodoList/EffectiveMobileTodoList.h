1c52d07466517d3d968c0f239c02bf9fee34d07e
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-03 10:57:51 +0300

    Did create new macOS framework for EffectiveMobileTodoListApp

---
+++ EffectiveMobileTodoList/EffectiveMobileTodoList/EffectiveMobileTodoList.h
@@ -0,0 +1,18 @@
+//
+//  EffectiveMobileTodoList.h
+//  EffectiveMobileTodoList
+//
+//  Created by Fenominall on 9/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EffectiveMobileTodoList.
+FOUNDATION_EXPORT double EffectiveMobileTodoListVersionNumber;
+
+//! Project version string for EffectiveMobileTodoList.
+FOUNDATION_EXPORT const unsigned char EffectiveMobileTodoListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EffectiveMobileTodoList/PublicHeader.h>
+
+
