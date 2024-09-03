17fdfcb3cd2117c986b310ec31f8ab45a763ff41
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-03 21:42:13 +0300

    Make sure that  'URLSessionHTTPClient 'performs GET request when using 'HTTPClient.get' method with url

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/URLSessionHTTPClientTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/URLSessionHTTPClientTests.swift
@@ -9,6 +9,52 @@ import Foundation
 import EffectiveMobileTodoList
 import XCTest
 
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    func get(from url: URL, completion: @escaping (Result) -> Void)
+}
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            
+        }
+        .resume()
+    }
+}
+
 class URLSessionHTTPClientTests: XCTestCase {
     
+    func test_getFromURL_performsGET() {
+        let url = anyURL()
+        let exp = expectation(description: "Wait for request")
+        
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        makeSUT().get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
