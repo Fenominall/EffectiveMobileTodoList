620cdb670f3d755331db857752e4eb40d42f9984
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-03 22:08:53 +0300

    Did move 'URLSessionHTTPClient' class  to production taget

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/URLSessionHTTPClientTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/URLSessionHTTPClientTests.swift
@@ -9,31 +9,6 @@ import Foundation
 import EffectiveMobileTodoList
 import XCTest
 
-public class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession) {
-        self.session = session
-    }
-    
-    private struct UnexpectedValuesRepresentation: Error {}
-    
-    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            completion(Result {
-                if let error = error {
-                    throw error
-                } else if let data = data, let response = response as? HTTPURLResponse {
-                    return (data, response)
-                } else {
-                    throw UnexpectedValuesRepresentation()
-                }
-            })
-        }
-        .resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGET() {
