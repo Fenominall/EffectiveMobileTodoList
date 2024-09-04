bdc07601a7694c5c8cf43c5f3a89f5f889b83b48
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 20:53:49 +0300

    Changed 'RemoteFeedLoader' initializer access level to public to be able to create it from another target

--- EffectiveMobileTodoList/EffectiveMobileTodoList/Feed API/RemoteFeedLoader.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoList/Feed API/RemoteFeedLoader.swift
@@ -11,7 +11,7 @@ public final class RemoteFeedLoader: TasksLoader {
     private let url: URL
     private let client: HTTPClient
     
-    init(url: URL, client: HTTPClient) {
+    public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
