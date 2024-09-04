500522f4fa91f8e550f34bbd265245f122ab06b4
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 22:19:21 +0300

    Add new testing group for 'FeedCache'
    - added new file in the 'FeedCache' to test Load Feed From Cache Use Case

---
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,14 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  EffectiveMobileTodoListTests
+//
+//  Created by Fenominall on 9/4/24.
+//
+
+import Foundation
+import EffectiveMobileTodoList
+import XCTest
+
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    
+}
