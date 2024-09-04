fddc6ee42d17f084d595e6b83f15e1dcc207b466
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 12:12:38 +0300

    Added a new file to  test the load use cases from remote

---
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/LoadFeedFromRemoteUseCaseTests.swift
@@ -0,0 +1,21 @@
+//
+//  LoadFeedFromRemoteUseCaseTests.swift
+//  EffectiveMobileTodoListTests
+//
+//  Created by Fenominall on 9/4/24.
+//
+
+import XCTest
+import EffectiveMobileTodoList
+
+public final class RemoteFeedLoader {
+    
+}
+
+class LoadFeedFromRemoteUseCaseTests: XCTest {
+    
+    func test_init_doesNotRequestDataFromURL() {
+    }
+}
+
+
