ecc62eb86a44dafbd1cfb0cf855243d0d778f518
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 17:35:30 +0300

    Added 'FeedTasksMapper' the class maps data and response to and return converted items as and array of TodoTask

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/LoadFeedFromRemoteUseCaseTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/LoadFeedFromRemoteUseCaseTests.swift
@@ -8,6 +8,51 @@
 import XCTest
 import EffectiveMobileTodoList
 
+public final class FeedTasksMapper {
+    private struct Root: Decodable {
+        let todos: [Item]
+        
+        var feed: [TodoTask] {
+            return todos.map { $0.item }
+        }
+    }
+    
+    private struct Item: Decodable {
+        let todo: String
+        let completed: Bool
+        
+        var item: TodoTask {
+            return TodoTask(
+                id: UUID(),
+                name: todo,
+                description: todo,
+                dateCreated: Date(),
+                status: completed
+            )
+        }
+    }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    private static let jsonDecoder = JSONDecoder()
+    
+    public static func map(
+        _ data: Data,
+        from response: HTTPURLResponse
+    ) throws -> [TodoTask] {
+        guard response.statusCode == 200 else {
+            throw Error.invalidData
+        }
+        
+        let root = try jsonDecoder.decode(Root.self, from: data)
+        
+        return root.feed
+    }
+}
+
+
 public final class RemoteFeedLoader: TasksLoader {
     private let url: URL
     private let client: HTTPClient
