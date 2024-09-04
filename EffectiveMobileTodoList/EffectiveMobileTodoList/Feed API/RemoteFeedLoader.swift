e818c3039c84ea03df3e23f2676f73076af6fde5
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 20:50:57 +0300

    Moved 'RemoteFeedLoader' to production

--- EffectiveMobileTodoList/EffectiveMobileTodoList/Feed API/RemoteFeedLoader.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoList/Feed API/RemoteFeedLoader.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+public final class RemoteFeedLoader: TasksLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public typealias LoadResult = TasksLoader.Result
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            do {
+                switch result {
+                case let .success((data, response)):
+                    let todos = try FeedTasksMapper.map(data, from: response)
+                    completion(.success(todos))
+                case .failure:
+                    completion(.failure(Error.connectivity))
+                }
+            } catch {
+                completion(.failure(Error.invalidData))
+            }
+        }
+    }
+}
