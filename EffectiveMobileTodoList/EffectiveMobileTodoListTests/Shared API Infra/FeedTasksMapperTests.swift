dc702a13c6d985102e896d2fcc1bd3e7c83683c6
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 21:50:27 +0300

    Add test to ensure FeedTasksMapper throws error for non-200 HTTP responses
    - Implemented `test_map_throwsErrorOnNon200HTTPResponse` to verify that `FeedTasksMapper` correctly throws an error when the HTTP response status code is not 200.
    - Tested various status codes including 199, 201, 300, 400, 404, and 500 to ensure consistent error handling across different failure scenarios.

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/FeedTasksMapperTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Shared API Infra/FeedTasksMapperTests.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import EffectiveMobileTodoList
+import XCTest
+
+class FeedTasksMapperTests: XCTestCase {
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+        let json = makeItemsJSON([])
+        
+        let samples = [199, 201, 300, 400, 404, 500]
+        
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try FeedTasksMapper
+                    .map(json,
+                         from: HTTPURLResponse(statusCode: code)))
+        }
+    }
+}
