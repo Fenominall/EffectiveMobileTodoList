1435443f48ef58fa4f13a0298fb882fdbae26998
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 16:50:09 +0300

    Make 'TodoTask' initializer public to be able to access it from another target

--- EffectiveMobileTodoList/EffectiveMobileTodoList/Tasks Feed Feature/TodoTask.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoList/Tasks Feed Feature/TodoTask.swift
@@ -13,4 +13,18 @@ public struct TodoTask: Equatable {
     public let description: String
     public let dateCreated: Date
     public let status: Bool
+    
+    public init(
+        id: UUID,
+        name: String,
+        description: String,
+        dateCreated: Date,
+        status: Bool
+    ) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.dateCreated = dateCreated
+        self.status = status
+    }
 }
