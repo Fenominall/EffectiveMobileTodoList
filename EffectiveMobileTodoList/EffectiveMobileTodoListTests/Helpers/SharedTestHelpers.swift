491bba09581ab23ec257bf931c19c0a9401af823
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-04 17:33:44 +0300

    Added helper method to create a dummy json data for testing purposes

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Helpers/SharedTestHelpers.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Helpers/SharedTestHelpers.swift
@@ -19,3 +19,37 @@ func anyNSError() -> NSError {
 func anyData() -> Data {
     return Data("invalid json".utf8)
 }
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["todos": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+func makeTask(
+    id: UUID = UUID(),
+    name: String,
+    description: String = "",
+    dateCreated: Date = Date(),
+    status: Bool
+) -> (model: TodoTask, json: [String: Any]) {
+    
+    // Create the TodoTask model
+    let task = TodoTask(
+        id: id,
+        name: name,
+        description: description,
+        dateCreated: dateCreated,
+        status: status
+    )
+    
+    // Create the corresponding JSON dictionary
+    let json: [String: Any] = [
+        "id": id.uuidString,
+        "todo": name,
+        "description": description,
+        "createdDate": ISO8601DateFormatter().string(from: dateCreated),
+        "completed": status
+    ]
+    
+    return (task, json)
+}
