b4420270665e346d5658e8081540414fc23b3fcc
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-05 07:19:01 +0300

    Add helper methods to LoadFeedFromCacheUseCaseTests
    - Added `makeSUT` helper to create instances of LocalFeedLoader and FeedStoreSpy with memory leak tracking.
    - Added `expect` helper to verify that `LocalFeedLoader` completes with the expected result when an action is performed.
    - Enhanced test readability and maintainability by simplifying test setup and assertions.

---
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,8 @@
+//
+//  FeedStoreSpy.swift
+//  EffectiveMobileTodoListTests
+//
+//  Created by Fenominall on 9/4/24.
+//
+
+import Foundation
--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -10,5 +10,35 @@ import EffectiveMobileTodoList
 import XCTest
 
 final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         file: StaticString = #filePath,
+                         line: UInt = #line)
+    -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
     
+    private func excpect(
+        _ sut: LocalFeedLoader,
+        toCompleteWith expectedResult: Result<[TodoTask], Error>,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line) {
+            
+            sut.load { receivedResult in
+                switch (receivedResult, expectedResult) {
+                case let (.success(receivedImages), .success(expectedImages)):
+                    XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+                case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                    XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+                }
+            }
+            action()
+        }
 }
