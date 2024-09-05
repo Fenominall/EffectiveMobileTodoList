12aeb4091ac13cc2bd0dfd061c5ee1a764a44403
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-05 10:57:29 +0300

    Add helper methods that allow to create mock 'TodoTask' and 'LocalTodoTask' models to be used for 'CacheFeedUserCaseTests'

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import EffectiveMobileTodoList
+
+func uniqueTodoTask() -> TodoTask {
+    TodoTask(
+        id: UUID(),
+        name: "Name1",
+        description: "Description1",
+        dateCreated: Date(),
+        status: true
+    )
+}
+
+func uniqueTodoTaskFeed() -> (models: [TodoTask], local: [LocalTodoTask]) {
+    let feed = [uniqueTodoTask(), uniqueTodoTask()]
+    let localFeedImages = feed.map {
+        LocalTodoTask(
+            id: $0.id,
+            name: $0.name,
+            description: $0.description,
+            dateCreated: $0.dateCreated,
+            status: $0.status
+        )
+    }
+    return (feed, localFeedImages)
+}
