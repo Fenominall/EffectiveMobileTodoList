d57f6949988faa32edaa9c1ab09e2628544340e8
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-05 15:08:05 +0300

    Add `DeleteFeedUseCaseTests` for testing task deletion in `LocalFeedLoader`
    - Introduced `DeleteFeedUseCaseTests` to verify the behavior of the task deletion use case in `LocalFeedLoader`.
    - Added the `makeSUT` helper method to create and configure a `LocalFeedLoader` and `FeedStoreSpy` for tests.
    - Added `excpect(_:toCompleteWithError:when:)` helper to verify deletion results, handling success and failure cases.
    - Set up error assertion to compare against expected NSError values in the deletion process.

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/DeleteFeedUseCaseTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/DeleteFeedUseCaseTests.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import XCTest
+import EffectiveMobileTodoList
+
+final class DeleteFeedUseCaseTests: XCTestCase {
+    
+    
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         file: StaticString = #filePath,
+                         line: UInt = #line)
+    -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func excpect(
+        _ sut: LocalFeedLoader,
+        toCompleteWithError expectedError: NSError?,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line) {
+            
+            sut.delete(selected: uniqueTodoTaskFeed().models) { result in
+                switch result {
+                case .success:
+                    XCTAssertNil(expectedError, "Expected error \(String(describing: expectedError)) but got success instead", file: file, line: line)
+                case .failure(let error as NSError):
+                    XCTAssertEqual(error, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Unexpected result \(result)", file: file, line: line)
+                }
+            }
+            action()
+        }
+}
