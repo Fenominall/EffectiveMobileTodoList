45b867acc2550cdd5da66d26da18b8bd944b63ee
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-05 11:15:37 +0300

    Add Add `CacheFeedUseCaseTests` class with helper methods for testing
    - Added `makeSUT` helper method to `CacheFeedUseCaseTests` to create instances of `LocalFeedLoader` and `FeedStoreSpy`.
    - Included memory leak tracking for both `store` and `sut`.

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import XCTest
+import EffectiveMobileTodoList
+
+final class CacheFeedUseCaseTests: XCTestCase {
+    
+    
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                         file: StaticString = #filePath,
+                         line: UInt = #line)
+    -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
