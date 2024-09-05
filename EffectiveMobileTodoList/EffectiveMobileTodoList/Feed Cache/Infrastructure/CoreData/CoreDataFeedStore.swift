8166e6a494b86bfc539cf8e30101665376b17bd1
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-05 21:39:01 +0300

    Add CoreData stack implementation for 'TasksFeedStore'
    - Implemented CoreDataFeedStore class to manage CoreData stack for TasksFeedStore.
    - Added persistent container loading with error handling.
    - Provided CoreData context for asynchronous operations.
    - Included helper extensions for loading the CoreData model and persistent container.
    - Ensured proper cleanup of persistent store references on deinitialization

--- EffectiveMobileTodoList/EffectiveMobileTodoList/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoList/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -5,4 +5,54 @@
 //  Created by Fenominall on 9/5/24.
 //
 
-import Foundation
+import CoreData
+
+public final class CoreDataFeedStore {
+    //MARK: - Properties
+    public static let storeURL = NSPersistentContainer
+        .defaultDirectoryURL()
+        .appending(path: "feed-store.sqlite")
+    
+    private static let modelName = "TasksFeedStore"
+    private static let model = NSManagedObjectModel.with(
+        name: modelName,
+        in: Bundle(for: CoreDataFeedStore.self)
+    )
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+    
+    //MARK: - Initialization
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataFeedStore.model else {
+            throw StoreError.modelNotFound
+        }
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
+    }
+    
+    func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
+    
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+    
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
+}
+
