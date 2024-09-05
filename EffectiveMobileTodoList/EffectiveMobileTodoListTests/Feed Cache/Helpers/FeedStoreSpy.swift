d125e35b4c27c64d958bde335b9360d01fdc0f33
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-05 07:44:16 +0300

    Implement FeedStoreSpy to test FeedStore interactions with CRUD operations
    - Added `FeedStoreSpy` class conforming to `FeedStore` protocol for testing.
    - Implemented `ReceivedMessages` enum to track CRUD actions: delete, insert, retrieve, and update.
    - Added methods to simulate the results of deletion, insertion, retrieval, and update operations.
    - Provided utility methods for completing each operation with success or failure to aid in unit tests.
    - Ensured that received messages are recorded and results are returned based on preset values.

--- EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -6,3 +6,102 @@
 //
 
 import Foundation
+import EffectiveMobileTodoList
+
+class FeedStoreSpy: FeedStore {
+    enum ReceivedMessages: Equatable {
+        case deleteCachedFeed([LocalTodoTask])
+        case insert([LocalTodoTask], Date)
+        case retrieve
+        case update(LocalTodoTask)
+    }
+    private (set) var receivedMessages = [ReceivedMessages]()
+    
+    private var deletionResult: FeedStore.DeletionResult?
+    private var insertionResult: FeedStore.InsertionResult?
+    private var retrievalResult: FeedStore.RetrievalResult?
+    private var updatingResult: FeedStore.UpdatingResult?
+    
+    // Deletion
+    func delete(
+        _ transactions: [LocalTodoTask],
+        completion: @escaping FeedStore.DeletionCompletion
+    ) {
+        receivedMessages.append(.deleteCachedFeed(transactions))
+        if let result = deletionResult {
+            completion(result)
+        }
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionResult = .failure(error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionResult = .success(())
+    }
+    
+    // Insertion
+    func insert(
+        _ tasks: [LocalTodoTask],
+        timestamp: Date,
+        completion: @escaping FeedStore.InsertionCompletion
+    ) {
+        receivedMessages.append(.insert(tasks, timestamp))
+        if let result = insertionResult {
+            completion(result)
+        }
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionResult = .failure(error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionResult = .success(())
+    }
+    
+    // Retrieval
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        receivedMessages.append(.retrieve)
+        if let result = retrievalResult {
+            completion(result)
+        }
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalResult = .failure(error)
+    }
+    
+    func completeRetrievalWithEmptyCache(at index: Int = 0) {
+        retrievalResult = .success(.none)
+    }
+    
+    func completeRetrieval(
+        with feed: [LocalTodoTask],
+        timestamp: Date,
+        at index: Int = 0) {
+            retrievalResult = .success(CachedFeed(feed: feed, timestamp: timestamp))
+        }
+    
+    // Updating
+    func update(
+        _ task: LocalTodoTask,
+        completion: @escaping FeedStore.UpdatingCompletion
+    ) {
+        receivedMessages.append(.update(task))
+        if let result = updatingResult {
+            completion(result)
+        }
+    }
+    
+    func completeUpdate(with error: Error, at index: Int = 0) {
+        updatingResult = .failure(error)
+    }
+    
+    func completeUpdateSuccessfully(at index: Int = 0) {
+        updatingResult = .success(())
+    }
+    
+}
+
