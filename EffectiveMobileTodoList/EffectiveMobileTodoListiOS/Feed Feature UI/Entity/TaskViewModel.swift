3844bf21c70f8102e3aaf470236f628268d4dcc6
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-08 22:33:49 +0300

    Changed the TaskViewModel properties names to match the name of the core model

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Feed Feature UI/Entity/TaskViewModel.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Feed Feature UI/Entity/TaskViewModel.swift
@@ -8,22 +8,22 @@
 import Foundation
 
 public struct TaskViewModel: Equatable {
-    public let id: String
+    public let id: UUID
     public let name: String
-    public let taskDescription: String
-    public let dateCreated: String
+    public let description: String
+    public let dateCreated: Date
     public let isCompleted: Bool
     
     public init(
-        id: String,
+        id: UUID,
         name: String,
-        taskDescription: String,
-        dateCreated: String,
+        description: String,
+        dateCreated: Date,
         isCompleted: Bool
     ) {
         self.id = id
         self.name = name
-        self.taskDescription = taskDescription
+        self.description = description
         self.dateCreated = dateCreated
         self.isCompleted = isCompleted
     }
