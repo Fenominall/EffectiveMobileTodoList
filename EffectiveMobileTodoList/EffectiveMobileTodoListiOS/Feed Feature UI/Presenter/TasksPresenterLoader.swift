456bd3470f0234f3cb6d98c848944d0aa82e2148
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-09 07:08:47 +0300

    Deleted the file

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Feed Feature UI/Presenter/TasksPresenterLoader.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Feed Feature UI/Presenter/TasksPresenterLoader.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+public protocol TasksPresenterLoader: AnyObject {
+    func viewDidLoad()
+}
---
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Feed Feature UI/Presenter/TasksPresenterProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  TasksPresenterProtocol.swift
+//  EffectiveMobileTodoListiOS
+//
+//  Created by Fenominall on 9/9/24.
+//
+
+import Foundation
