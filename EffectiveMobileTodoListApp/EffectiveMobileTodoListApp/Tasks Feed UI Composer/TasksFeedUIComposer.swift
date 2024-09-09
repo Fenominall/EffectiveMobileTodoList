6ddd5c84121370a4ce0262daeace745d4034d8e1
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-09 22:17:47 +0300

    Implement TasksFeedUIComposer and TasksFeedViewAdapter for task feed composition and UI adaptation.
    - Added `TasksFeedUIComposer` to compose the task feed with a loader and selection handler.
    - Integrated `TasksFeedViewAdapter` to map and display task view models in the `TaskListViewController`.
    - Ensured task selection is handled via closure callbacks to allow interaction with the task feed.
    - Applied weak references to avoid retain cycles between the UI components and the presenter.

--- EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Tasks Feed UI Composer/TasksFeedUIComposer.swift
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Tasks Feed UI Composer/TasksFeedUIComposer.swift
@@ -16,62 +16,24 @@ final class TasksFeedUIComposer {
         feedLoader: TasksLoader,
         selection: @escaping (TodoTask) -> Void
     ) -> TaskListViewController {
+        let view = TaskListViewController()
         let interactor = TasksInteractor(
             loader: MainQueueDispatchDecorator(decoratee: feedLoader)
         )
-        let tasksVC = TaskListViewController()
         
         let presenter = TasksPresenter(
             view: WeakRefVirtualProxy(
                 TasksFeedViewAdapter(
-                    controller: tasksVC,
+                    controller: view,
                     selection: selection)),
-            errorView: WeakRefVirtualProxy(tasksVC),
-            loadingView: WeakRefVirtualProxy(tasksVC),
+            errorView: WeakRefVirtualProxy(view),
+            loadingView: WeakRefVirtualProxy(view),
             interactor: WeakRefVirtualProxy(interactor)
         )
         
-        tasksVC.onRefresh = presenter.viewDidLoad
+        view.onRefresh = presenter.viewDidLoad
         interactor.presenter = presenter
         
-        return tasksVC
-    }
-}
-
-
-final class TasksFeedViewAdapter: TasksView {
-    private weak var controller: TaskListViewController?
-    private let selection: (TodoTask) -> Void
-    
-    init(controller: TaskListViewController,
-         selection: @escaping (TodoTask) -> Void) {
-        self.controller = controller
-        self.selection = selection
-    }
-    
-    func displayTasks(_ viewModel: [EffectiveMobileTodoListiOS.TodoTaskViewModel]) {
-        controller?.tableModel = viewModel.map { [weak self] model in
-            guard let self = self else {
-                return TasksTableCellController(viewModel: model,
-                                                selection: {
-                }) }
-            
-            return TasksTableCellController(viewModel: model,
-                                            selection: {
-                [selection] in
-                selection(self.mapToTodoTask(from: model))
-            })
-            
-        }
-    }
-    
-    private func mapToTodoTask(from dto: TodoTaskViewModel) -> TodoTask {
-        return TodoTask(
-            id: dto.id,
-            name: dto.name,
-            description: dto.description,
-            dateCreated: dto.dateCreated,
-            status: dto.isCompleted
-        )
+        return view
     }
 }
--- EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Tasks Feed UI Composer/TasksFeedViewAdapter.swift
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Tasks Feed UI Composer/TasksFeedViewAdapter.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import EffectiveMobileTodoList
+import EffectiveMobileTodoListiOS
+
+final class TasksFeedViewAdapter: TasksView {
+    private weak var controller: TaskListViewController?
+    private let selection: (TodoTask) -> Void
+    
+    init(controller: TaskListViewController,
+         selection: @escaping (TodoTask) -> Void) {
+        self.controller = controller
+        self.selection = selection
+    }
+    
+    func displayTasks(_ viewModel: [EffectiveMobileTodoListiOS.TodoTaskViewModel]) {
+        controller?.tableModel = viewModel.map { [weak self] model in
+            guard let self = self else {
+                return TasksTableCellController(viewModel: model,
+                                                selection: {
+                }) }
+            
+            return TasksTableCellController(viewModel: model,
+                                            selection: {
+                [selection] in
+                selection(self.mapToTodoTask(from: model))
+            })
+        }
+    }
+    
+    private func mapToTodoTask(from dto: TodoTaskViewModel) -> TodoTask {
+        return TodoTask(
+            id: dto.id,
+            name: dto.name,
+            description: dto.description,
+            dateCreated: dto.dateCreated,
+            status: dto.isCompleted
+        )
+    }
+}
