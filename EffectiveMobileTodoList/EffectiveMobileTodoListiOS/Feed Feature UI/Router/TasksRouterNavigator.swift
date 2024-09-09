c44e0a65a7d4e2e847e5bdc8c386e5c063cce097
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-09 09:30:02 +0300

    Added 'TasksRouterNavigator' protocol to the project

---
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Feed Feature UI/Router/TasksRouterNavigator.swift
@@ -0,0 +1,12 @@
+//
+//  TasksRouterNavigator.swift
+//  EffectiveMobileTodoListiOS
+//
+//  Created by Fenominall on 9/9/24.
+//
+
+import Foundation
+
+public protocol TasksRouterNavigator {
+    func navigateToTaskAddEditController()
+}
