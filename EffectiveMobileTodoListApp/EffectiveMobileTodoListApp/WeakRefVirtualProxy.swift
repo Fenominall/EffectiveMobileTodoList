5d0b1f10b158835bfe9a98a8d359adeda366a1b9
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-09 21:11:45 +0300

    Add WeakRefVirtualProxy to avoid strong reference cycles in tasks flow
    - Implemented `WeakRefVirtualProxy` to hold weak references to objects conforming to `TaskLoadingView`, `TaskErrorView`, `TasksView`, and `TasksInteractorOutput` protocols
    - Ensures that views and interactor outputs are not strongly retained, preventing memory leaks
    - Added extensions for proxying methods from `TasksInteractorOutput` and related views

--- EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/WeakRefVirtualProxy.swift
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/WeakRefVirtualProxy.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import EffectiveMobileTodoListiOS
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: TaskLoadingView where T: TaskLoadingView {
+    func display(_ viewModel: EffectiveMobileTodoListiOS.TaskLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: TaskErrorView where T: TaskErrorView {
+    func display(_ viewModel: EffectiveMobileTodoListiOS.TaskErrorViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: TasksView where T: TasksView {
+    func displayTasks(_ viewModel: [TodoTaskViewModel]) {
+        object?.displayTasks(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: TasksInteractorOutput where T: TasksInteractorOutput {
+    func didStartOperation() {
+        object?.didStartOperation()
+    }
+    
+    func didLoadTasks(_ tasks: [EffectiveMobileTodoListiOS.TodoTaskViewModel]) {
+        object?.didLoadTasks(tasks)
+    }
+    
+    func didFinish(with error: any Error) {
+        object?.didFinish(with: error)
+    }
+    
+    func didSaveTasks() {
+        object?.didSaveTasks()
+    }
+}
