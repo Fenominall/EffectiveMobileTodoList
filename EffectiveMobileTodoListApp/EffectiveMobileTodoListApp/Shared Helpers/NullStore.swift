0d4a303d978606727d45e3aea626df81d1ce628b
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-09 22:32:09 +0300

    Implement NullStore with update method returning success for no-operation

---
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Shared Helpers/NullStore.swift
@@ -0,0 +1,27 @@
+//
+//  NullStore.swift
+//  EffectiveMobileTodoListApp
+//
+//  Created by Fenominall on 9/9/24.
+//
+
+import Foundation
+import EffectiveMobileTodoList
+
+class NullStore: FeedStore {
+    func delete(_ task: EffectiveMobileTodoList.LocalTodoTask, completion: @escaping DeletionCompletion) {
+        completion(.success(()))
+    }
+    
+    func insert(_ tasks: [EffectiveMobileTodoList.LocalTodoTask], completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.none))
+    }
+    
+    func update(_ task: EffectiveMobileTodoList.LocalTodoTask, completion: @escaping (UpdatingResult) -> Void) {
+        completion(.success(()))
+    }
+}
