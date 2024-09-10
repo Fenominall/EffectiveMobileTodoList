6a23568f6128309508271bb26c976c3e79e46d5d
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-10 09:19:42 +0300

    Configure SceneDelegate with lazy properties for HTTP client, store, and loaders
    - Added lazy initialization for HTTPClient and CoreDataFeedStore in SceneDelegate.
    - Set up remote and local feed loaders using lazy properties.
    - Initialized navigationController with TasksFeedUIComposer for rootViewController.
    - Implemented configureWindow method to set up and display the main window.

--- EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/SceneDelegate.swift
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/SceneDelegate.swift
@@ -6,18 +6,53 @@
 //
 
 import UIKit
+import EffectiveMobileTodoList
+import EffectiveMobileTodoListiOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private lazy var httpClient: HTTPClient = {
+        URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    }()
+    
+    private lazy var store: FeedStore = {
+        do {
+            return try CoreDataFeedStore(
+                storeURL: CoreDataFeedStore.storeURL
+            )
+        } catch {
+            print("Error instantiating CoreData store: \(error.localizedDescription)")
+            assertionFailure("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
+            return NullStore()
+        }
+    }()
+    
+    private lazy var remoteURL = URL(string: "https://dummyjson.com/todos")!
+    private lazy var remoteFeedLoader: RemoteFeedLoader = {
+        RemoteFeedLoader(url: remoteURL, client: httpClient)
+    }()
+    private lazy var localFeedLoader: LocalFeedLoader = {
+        LocalFeedLoader(store: store)
+    }()
+    
+    private lazy var navigationController = UINavigationController(
+        rootViewController: TasksFeedUIComposer
+            .tasksFeedComposedWith(
+                feedLoader: remoteFeedLoader,
+                selection: { _ in }
+            )
+    )
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        configureWindow()
     }
 
+    private func configureWindow() {
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
 }
-
