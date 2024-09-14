df5b675f10eba9d65d64031f81f20f3a9000bf56
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-14 20:14:44 +0300

    Added AddEditTodoTaskViewModell for the AddEditTaskViewController to be used

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Entity/AddEditTodoTaskViewModel.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Entity/AddEditTodoTaskViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+public struct AddEditTodoTaskViewModel: Equatable {
+    public let id: UUID
+    public let name: String
+    public let description: String
+    public let dateCreated: Date
+    public var isCompleted: Bool
+    public let startTime: Date?
+    public let endTime: Date?
+    
+    var selectedStartTime: Date?
+    var selectedEndTime: Date?
+    
+    public init(
+        id: UUID,
+        name: String,
+        description: String,
+        dateCreated: Date,
+        isCompleted: Bool,
+        startTime: Date? = nil,
+        endTime: Date? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.dateCreated = dateCreated
+        self.isCompleted = isCompleted
+        self.startTime = startTime
+        self.endTime = endTime
+    }
+}
