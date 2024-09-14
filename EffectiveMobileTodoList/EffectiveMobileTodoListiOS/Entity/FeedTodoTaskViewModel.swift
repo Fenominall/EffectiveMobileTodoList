032afd1821bf95057f270cc49eb0a00cb4face19
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-14 20:00:27 +0300

    Added startTime and endTime two optional properties to represent the specific time for the Task

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Entity/FeedTodoTaskViewModel.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Entity/FeedTodoTaskViewModel.swift
@@ -13,18 +13,24 @@ public struct FeedTodoTaskViewModel: Equatable {
     public let description: String
     public let dateCreated: Date
     public var isCompleted: Bool
+    public let startTime: Date?
+    public let endTime: Date?
     
     public init(
         id: UUID,
         name: String,
         description: String,
         dateCreated: Date,
-        isCompleted: Bool
+        isCompleted: Bool,
+        startTime: Date? = nil,
+        endTime: Date? = nil
     ) {
         self.id = id
         self.name = name
         self.description = description
         self.dateCreated = dateCreated
         self.isCompleted = isCompleted
+        self.startTime = startTime
+        self.endTime = endTime
     }
 }
