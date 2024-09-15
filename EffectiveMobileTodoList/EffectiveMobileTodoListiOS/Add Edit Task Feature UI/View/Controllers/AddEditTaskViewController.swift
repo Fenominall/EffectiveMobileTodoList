68e117d16f9e16ac82e32005ffd2d9ca2b75f02f
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-15 08:54:16 +0300

    Added to 'AddEditTaskViewController' a dependency as AddEditTodoTaskViewModel with the init

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Add Edit Task Feature UI/View/Controllers/AddEditTaskViewController.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Add Edit Task Feature UI/View/Controllers/AddEditTaskViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 public class AddEditTaskViewController: UIViewController {
-    var editingTaskViewModel: FeedTodoTaskViewModel?
+    public let editingTaskViewModel: AddEditTodoTaskViewModel
     
     // MARK: - UI Elements
     private let taskNameTextField = makeTextField(with: "Task Name")
@@ -119,6 +119,15 @@ public class AddEditTaskViewController: UIViewController {
     }
     
     // MARK: - Lifecycle
+    public init(editingTaskViewModel: AddEditTodoTaskViewModel) {
+        self.editingTaskViewModel = editingTaskViewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
