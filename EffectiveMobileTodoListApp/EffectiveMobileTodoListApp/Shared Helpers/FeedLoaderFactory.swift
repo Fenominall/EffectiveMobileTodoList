b3d80d886b93c6d7e166d52b627aee4f7967d640
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-17 07:24:43 +0300

    Create FeedLoaderFactory for managing data retrieval
    - Establishes factory class to manage feed loader creation.
    - Provides methods for building `RemoteFeedLoader` and `LocalFeedLoader` instances.
    - Implements `makeFeedLoader` to handle initial load strategy (cached vs fresh data) based on first launch.
    - Introduces `makeCacheDecorator` to add caching functionality to any loader.

--- EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Shared Helpers/FeedLoaderFactory.swift
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Shared Helpers/FeedLoaderFactory.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import EffectiveMobileTodoList
+
+final class FeedLoaderFactory {
+    private let httpClient: HTTPClient
+    private let store: FeedStore
+    private let remoteURL: URL
+    private let firstLaunchManager: FirstLaunchManager
+    
+    init(
+        httpClient: HTTPClient,
+        store: FeedStore,
+        remoteURL: URL,
+        firstLaunchManager: FirstLaunchManager
+    ) {
+        self.httpClient = httpClient
+        self.store = store
+        self.remoteURL = remoteURL
+        self.firstLaunchManager = firstLaunchManager
+    }
+    
+    func makeRemoteFeedLoader() -> RemoteFeedLoader {
+        return RemoteFeedLoader(url: remoteURL, client: httpClient)
+    }
+    
+    func makeLocalFeedLoader() -> LocalFeedLoader {
+        return LocalFeedLoader(store: store)
+    }
+    
+    func makeFeedLoader() -> TasksLoader {
+        let remoteLoader = makeRemoteFeedLoader()
+        let localLoader = makeLocalFeedLoader()
+        
+        if firstLaunchManager.isFirstLaunch() {
+            return TasksFeedLoaderWithFallbackComposite(
+                primary: makeCacheDecorator(decoratee: remoteLoader),
+                fallback: localLoader)
+        } else {
+            return localLoader
+        }
+    }
+    
+    func makeCacheDecorator(decoratee: TasksLoader) -> TasksFeedLoaderCacheDecorator {
+        return TasksFeedLoaderCacheDecorator(decoratee: decoratee, cache: makeLocalFeedLoader())
+    }
+}
