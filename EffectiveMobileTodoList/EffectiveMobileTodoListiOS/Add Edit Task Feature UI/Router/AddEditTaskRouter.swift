fd574d800c1f9a1e3c762e0c39b80dd9e38627fa
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-18 21:58:02 +0300

    feat: Add AddEditTaskNavigationRouter to handle routing
    This commit creates the AddEditTaskNavigationRouter, which provides a
    method to route back to the tasks feed. It uses the navigation controller
    to pop the current view controller.

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Add Edit Task Feature UI/Router/AddEditTaskRouter.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Add Edit Task Feature UI/Router/AddEditTaskRouter.swift
@@ -5,4 +5,16 @@
 //  Created by Fenominall on 9/18/24.
 //
 
-import Foundation
+import UIKit
+
+public final class AddEditTaskRouter: AddEditTaskNavigationRouter {
+    weak var controller: UIViewController?
+    
+    public init(controller: UIViewController? = nil) {
+        self.controller = controller
+    }
+    
+    public func routeToTasksFeed() {
+        controller?.navigationController?.popViewController(animated: true)
+    }
+}
