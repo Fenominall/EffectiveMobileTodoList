55dff41209f626ec67cd137c7abf4be2dc075592
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-18 21:56:59 +0300

    feat: Implement AddEditTaskPresenter for task management
    This commit introduces the AddEditTaskPresenter, responsible for
    interacting with the interactor and router to manage task operations.

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Add Edit Task Feature UI/Presenter/AddEditTaskPresenter.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOS/Add Edit Task Feature UI/Presenter/AddEditTaskPresenter.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import EffectiveMobileTodoList
+
+public final class AddEditTaskPresenter {
+    private let interactor: AddEditTaskInteractor
+    private let router: AddEditTaskNavigationRouter
+    
+    public init(
+        interactor: AddEditTaskInteractor,
+        router: AddEditTaskNavigationRouter
+    ) {
+        self.interactor = interactor
+        self.router = router
+    }
+}
+
+extension AddEditTaskPresenter: AddEditTaskInteractorOutput {
+    public func didSaveTask(_ task: EffectiveMobileTodoList.TodoTask) {
+        interactor.deleteTask(task)
+    }
+    
+    public func didDeleteTask(_ task: EffectiveMobileTodoList.TodoTask) {
+        interactor.deleteTask(task)
+    }
+    
+    public func didUpdateTask(_ task: EffectiveMobileTodoList.TodoTask) {
+        interactor.update(task)
+    }
+    
+    public func didFinishWithError(_ error: any Error) {
+        
+    }
+}
