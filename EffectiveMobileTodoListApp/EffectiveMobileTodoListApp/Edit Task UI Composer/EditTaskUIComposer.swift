6e8cd8c3ea50f64c0dfecf7668d7e4473a28ccf9
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-19 11:59:46 +0300

    Implement EditTaskUIComposer for task editing functionality
    - Created a static method `composedWith` to configure `AddEditTodoTaskViewController` for editing a selected TodoTask.
    - Initialized the view model with the selected task and set up dependencies for saving and removing tasks.
    - Configured the interactor, presenter, and router for handling task updates and deletions.
    - Assigned closures for handling task updates and deletions through the presenter.

--- EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Edit Task UI Composer/EditTaskUIComposer.swift
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Edit Task UI Composer/EditTaskUIComposer.swift
@@ -12,9 +12,26 @@ import EffectiveMobileTodoListiOS
 final class EditTaskUIComposer {
     private init() {}
     
-    static func composedWith(selectedModel: TodoTask) -> AddEditTodoTaskViewController {
-        let viewModel = AddEditTodoTaskViewModel(task: selectedModel)
+    static func composedWith(
+        selectedModel: TodoTask,
+        taskSaver: TaskSaver,
+        taskRemover: TasksRemover
+    ) -> AddEditTodoTaskViewController {
+        var viewModel = AddEditTodoTaskViewModel(task: selectedModel)
         let controller = AddEditTodoTaskViewController(viewModel: viewModel)
+        let router = AddEditTaskRouter(controller: controller)
+        
+        let interactor = AddEditTaskInteractor(taskSaver: taskSaver, taskRemover: taskRemover)
+        let presenter = AddEditTaskPresenter(interactor: interactor, router: router)
+        
+        interactor.presenter = presenter
+    
+        viewModel.onSaveUpdateTransaction = presenter.didUpdateTask
+        viewModel.deletionHandler = { [weak presenter] in
+            presenter?.didDeleteTask(selectedModel)
+        }
+        
+        
         return controller
     }
 }
