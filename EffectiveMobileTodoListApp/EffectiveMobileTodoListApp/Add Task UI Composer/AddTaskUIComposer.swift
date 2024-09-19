5cfdd47b0472651359682108a233554baa3bc4f9
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-19 11:59:01 +0300

    Refactor AddTaskUIComposer to initialize AddEditTodoTaskViewController with view model and dependency injection
    - Created a static method `composedWith` to set up the view controller with task saving and removing capabilities.
    - Configured the interactor, presenter, and router for handling task operations.
    - Assigned the onSaveAddTransaction closure to the presenter for handling task saving.

--- EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Add Task UI Composer/AddTaskUIComposer.swift
+++ EffectiveMobileTodoListApp/EffectiveMobileTodoListApp/Add Task UI Composer/AddTaskUIComposer.swift
@@ -12,9 +12,21 @@ import EffectiveMobileTodoListiOS
 final class AddTaskUIComposer {
     private init() {}
     
-    static func composedWith() -> AddEditTodoTaskViewController {
-        let viewModel = AddEditTodoTaskViewModel()
+    static func composedWith(taskSaver: TaskSaver, taskRemover: TasksRemover) -> AddEditTodoTaskViewController {
+        var viewModel = AddEditTodoTaskViewModel()
         let controller = AddEditTodoTaskViewController(viewModel: viewModel)
+        let router = AddEditTaskRouter(controller: controller)
+        
+        let interactor = AddEditTaskInteractor(
+            taskSaver: MainQueueDispatchDecorator(decoratee: taskSaver),
+            taskRemover: MainQueueDispatchDecorator(decoratee: taskRemover)
+        )
+        let presenter = AddEditTaskPresenter(interactor: interactor, router: router)
+        
+        interactor.presenter = presenter
+        
+        viewModel.onSaveAddTransaction = presenter.didSaveTask
+        
         return controller
     }
 }
