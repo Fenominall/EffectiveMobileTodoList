74b3f385a1b432104f9e5772306f27b548dc7afa
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-22 17:56:42 +0300

    Modifed makeSUT method for the TasksFeedInteractorTests to return TasksFeedInteractor and MockLoaderSpy to perform for the tests
    added expectation helper methods for each Interactor
    
    potentially will be separated for differentet use cases with separate tests
    
    For deletion, updating, loading, saving

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/InteracgotTestCase/TaskFeedInteractorTestCase.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/InteracgotTestCase/TaskFeedInteractorTestCase.swift
@@ -6,12 +6,25 @@
 //
 
 import XCTest
+import EffectiveMobileTodoList
 import EffectiveMobileTodoListiOS
 
 final class TasksFeedInteractorTests: XCTestCase {
     
+    func test_loadTasks_callsLoaderAndPassesTasksToPresenterOnSuccess() {
+        let (sut, loaderSpy) = makeSUT()
+        
+        sut.loadTasks()
+        loaderSpy.completeLoading(with: [uniqueTodoTask()])
+        
+        
+    }
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) ->  TasksFeedInteractor {
+    // MARK: - Helpers
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) ->  (interactor: TasksFeedInteractor, loader: MockLoaderSpy) {
         let loaderSpy = MockLoaderSpy()
         let presenterSpy = MockPresenterSpy()
         
@@ -20,6 +33,86 @@ final class TasksFeedInteractorTests: XCTestCase {
         trackForMemoryLeaks(loaderSpy, file: file, line: line)
         trackForMemoryLeaks(presenterSpy, file: file, line: line)
         trackForMemoryLeaks(interactor, file: file, line: line)
-        return interactor
+        return (interactor, loaderSpy)
     }
+    
+    private func excpect(
+        _ sut: MockLoaderSpy,
+        toCompleteWith expectedResult: Result<[TodoTask], Error>,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line) {
+            
+            sut.load { receivedResult in
+                switch (receivedResult, expectedResult) {
+                case let (.success(receivedImages), .success(expectedImages)):
+                    XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+                case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                    XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+                }
+            }
+            action()
+        }
+    
+    private func excpect(
+        _ sut: MockLoaderSpy,
+        toCompleteWithError expectedError: NSError?,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line) {
+            
+            sut.save(uniqueTodoTaskFeed().models) { result in
+                switch result {
+                case .success:
+                    XCTAssertNil(expectedError, "Expected error \(String(describing: expectedError)) but got success instead", file: file, line: line)
+                case .failure(let error as NSError):
+                    XCTAssertEqual(error, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Unexpected result \(result)", file: file, line: line)
+                }
+            }
+            action()
+        }
+    
+    private func excpectDelete(
+        _ sut: MockLoaderSpy,
+        toCompleteWithError expectedError: NSError?,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line) {
+            
+            sut.delete(selected: uniqueTodoTask()) { result in
+                switch result {
+                case .success:
+                    XCTAssertNil(expectedError, "Expected error \(String(describing: expectedError)) but got success instead", file: file, line: line)
+                case .failure(let error as NSError):
+                    XCTAssertEqual(error, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Unexpected result \(result)", file: file, line: line)
+                }
+            }
+            action()
+        }
+    
+    private func excpect(
+        _ sut: LocalFeedLoader,
+        toCompleteWithError expectedError: NSError?,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line) {
+            
+            sut.update(uniqueTodoTask()) { result in
+                switch result {
+                case .success:
+                    XCTAssertNil(expectedError, "Expected error \(String(describing: expectedError)) but got success instead", file: file, line: line)
+                case .failure(let error as NSError):
+                    XCTAssertEqual(error, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Unexpected result \(result)", file: file, line: line)
+                }
+            }
+            action()
+        }
 }
