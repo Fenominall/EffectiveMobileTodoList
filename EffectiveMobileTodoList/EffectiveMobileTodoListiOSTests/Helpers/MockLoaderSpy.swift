1cf318d7c5384474492f5ed7ac24c5ddf6bb3eb3
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-22 17:54:00 +0300

    Modified MockLoaderSpy to receive the messages about completions when peforming client actions

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/Helpers/MockLoaderSpy.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/Helpers/MockLoaderSpy.swift
@@ -10,35 +10,110 @@ import EffectiveMobileTodoList
 import EffectiveMobileTodoListiOS
 
 final class MockLoaderSpy: TasksLoader {
-    private var loadMessages = [(TasksLoader.Result) -> Void]()
-    private var saveTasksMessages = [(SaveResult) -> Void]()
-    private var saveTaskMessages = [(SaveResult) -> Void]()
-    private var updateTaskMessages = [(SaveResult) -> Void]()
-    private var deleteTaskMessages = [(DeletionResult) -> Void]()
-        
-    func load(completion: @escaping (TasksLoader.Result) -> Void) {
-        loadMessages.append(completion)
+    enum ReceivedMessages: Equatable {
+        case deleteCachedFeed(TodoTask)
+        case insert([TodoTask])
+        case insert(task: TodoTask)
+        case retrieve
+        case update(TodoTask)
     }
+    private(set) var receivedMessages = [ReceivedMessages]()
+    
+    private var deletionResult: TasksRemover.DeletionResult?
+    private var insertionResult: TaskSaver.SaveResult?
+    private var feedInsertionResult: TasksFeedCache.SaveResult?
+    private var retrievalResult: TasksLoader.Result?
+    private var updatingResult: TaskSaver.SaveResult?
+        
+    
+    // Loading
+    func load(completion: @escaping (TasksLoader.Result) -> Void) {
+        receivedMessages.append(.retrieve)
+        if let result = retrievalResult {
+            completion(result)
+        }
+    }
+    
+    func completeLoading(with error: Error, at index: Int = 0) {
+        retrievalResult = .failure(error)
+    }
+    
+    func completeLoadingWithEmptyCache(at index: Int = 0) {
+        retrievalResult = .success([])
+    }
+    
+    func completeLoading(
+        with feed: [TodoTask],
+        at index: Int = 0) {
+            retrievalResult = .success(feed)
+        }
 }
 
+// Saving
 extension MockLoaderSpy: TaskSaver {
     func save(_ task: EffectiveMobileTodoList.TodoTask, completion: @escaping (SaveResult) -> Void) {
-        saveTaskMessages.append(completion)
+        receivedMessages.append(.insert(task: task))
+        if let result = insertionResult {
+            completion(result)
+        }
+    }
+    
+    func completeTaskInsertion(with error: Error, at index: Int = 0) {
+        insertionResult = .failure(error)
+    }
+    
+    func completeTaskInsertionSuccessfully(at index: Int = 0) {
+        insertionResult = .success(())
     }
     
     func update(_ task: EffectiveMobileTodoList.TodoTask, completion: @escaping (SaveResult) -> Void) {
-        updateTaskMessages.append(completion)
+        receivedMessages.append(.update(task))
+        if let result = updatingResult {
+            completion(result)
+        }
+    }
+    
+    func completeUpdating(with error: Error, at index: Int = 0) {
+        insertionResult = .failure(error)
+    }
+    
+    func completeUpdatingSuccessfully(at index: Int = 0) {
+        insertionResult = .success(())
     }
 }
 
+// Save Feed
 extension MockLoaderSpy: TasksFeedCache {
     func save(_ feed: [TodoTask], completion: @escaping (SaveResult) -> Void) {
-        saveTasksMessages.append(completion)
+        receivedMessages.append(.insert(feed))
+        if let result = feedInsertionResult {
+            completion(result)
+        }
+    }
+    
+    func completeFeedInsertion(with error: Error, at index: Int = 0) {
+        insertionResult = .failure(error)
+    }
+    
+    func completeFeedInsertionSuccessfully(at index: Int = 0) {
+        insertionResult = .success(())
     }
 }
 
+// Delete
 extension MockLoaderSpy: TasksRemover {
     func delete(selected task: EffectiveMobileTodoList.TodoTask, completion: @escaping (DeletionResult) -> Void) {
-        deleteTaskMessages.append(completion)
+        receivedMessages.append(.deleteCachedFeed(task))
+        if let result = deletionResult {
+            completion(result)
+        }
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionResult = .failure(error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionResult = .success(())
     }
 }
