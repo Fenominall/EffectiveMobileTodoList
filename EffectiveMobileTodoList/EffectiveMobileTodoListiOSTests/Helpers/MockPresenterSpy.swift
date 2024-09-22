c1f81f81cdd6d77afb63a98a884985efcf82e9de
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-22 10:29:45 +0300

    Adds a MockPresenterSpy class that conforms to the TasksFeedInteractorOutput protocol. Provides properties to track the number of times various methods are called, as well as storing loaded tasks and errors for verification.

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/Helpers/MockPresenterSpy.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/Helpers/MockPresenterSpy.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import EffectiveMobileTodoList
+import EffectiveMobileTodoListiOS
+
+final class MockPresenterSpy: TasksFeedInteractorOutput {
+    var didStartOperationCallCount = 0
+    var didLoadTasksCallCount = 0
+    var didFinishWithErrorCallCount = 0
+    var didFinishOperationCallCount = 0
+    var didSelectAddNewTaskCallCount = 0
+    var loadedTasks: [TodoTask] = []
+    var receivedError: Error?
+    
+    func didStartOperation() {
+        didStartOperationCallCount += 1
+    }
+    
+    func didLoadTasks(_ tasks: [EffectiveMobileTodoList.TodoTask]) {
+        didLoadTasksCallCount += 1
+        self.loadedTasks = tasks
+    }
+    
+    func didSelectAddNewTask() {
+        didSelectAddNewTaskCallCount += 1
+    }
+    
+    func didFinish(with error: any Error) {
+        didFinishWithErrorCallCount += 1
+        receivedError = error
+    }
+    
+    func didFinishOperation() {
+        didFinishOperationCallCount += 1
+    }
+}
