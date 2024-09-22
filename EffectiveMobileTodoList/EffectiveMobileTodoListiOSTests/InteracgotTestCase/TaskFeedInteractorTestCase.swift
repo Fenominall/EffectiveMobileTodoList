f79076ad9c5ea5aa6bdd32137f0c09bd401ce58b
Vladislav Todorov <it.vtodorov@gmail.com>
2024-09-22 10:30:36 +0300

    Added class for TasksFeedInteractorTests to test the Loader, Interactor and Presenter
    Added helper method make SUT to compose all necessary components

--- EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/InteracgotTestCase/TaskFeedInteractorTestCase.swift
+++ EffectiveMobileTodoList/EffectiveMobileTodoListiOSTests/InteracgotTestCase/TaskFeedInteractorTestCase.swift
@@ -5,4 +5,21 @@
 //  Created by Fenominall on 9/22/24.
 //
 
-import Foundation
+import XCTest
+import EffectiveMobileTodoListiOS
+
+final class TasksFeedInteractorTests: XCTestCase {
+    
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) ->  TasksFeedInteractor {
+        let loaderSpy = MockLoaderSpy()
+        let presenterSpy = MockPresenterSpy()
+        
+        let interactor = TasksFeedInteractor(loader: loaderSpy, remover: loaderSpy, taskSaver: loaderSpy)
+        interactor.presenter = presenterSpy
+        trackForMemoryLeaks(loaderSpy, file: file, line: line)
+        trackForMemoryLeaks(presenterSpy, file: file, line: line)
+        trackForMemoryLeaks(interactor, file: file, line: line)
+        return interactor
+    }
+}
